2fc59951be3cb2512524dd4e71738f59e714f61c
franklin <frank.fkn.23@gmail.com>
2025-07-30 12:44:27 -0300

    modelagem de usuario no banco

---
+++ gw_team/models/users.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, onupdate=func.now(), server_default=func.now()
+    )
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    last_name: Mapped[str]
+    password: Mapped[str]
