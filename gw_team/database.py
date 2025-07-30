99bfa0d4f0d27c3e42ab9ec0e15a046728b5969c
franklin <frank.fkn.23@gmail.com>
2025-07-30 12:39:21 -0300

    config do banco

---
+++ gw_team/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from gw_team.settings import Settings
+
+engine = create_engine(Settings().DATABASE_URL)
+
+
+def db_session():  # pragma: no cover
+    with Session(engine, expire_on_commit=False) as session:
+        yield session
