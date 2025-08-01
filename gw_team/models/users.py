a7583af398022fb5808fcb14a0f939f707bd7225
franklin <frank.fkn.23@gmail.com>
2025-08-01 09:32:16 -0300

    add user_type no model

--- gw_team/models/users.py
+++ gw_team/models/users.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
 
+
 class UserType(str, Enum):
     admin = 'admin'
-    student = 'client'
+    client = 'client'
 
 
 @table_registry.mapped_as_dataclass
--- gw_team/schemas/users.py
+++ gw_team/schemas/users.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
+
+from gw_team.models.users import UserType
 
 
 class User(BaseModel):
     name: str
     last_name: str
     email: EmailStr
+    user_type: UserType
 
 
 class UserPublic(User):
     id: int
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UserSchema(User):
