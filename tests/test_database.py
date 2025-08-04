229628287990eca2081e60f08ec6705ee15dd4a3
franklin <frank.fkn.23@gmail.com>
2025-08-04 15:56:37 -0300

    teste de criacao do usuario no banco

---
+++ tests/test_database.py
@@ -0,0 +1,54 @@
+from dataclasses import asdict
+
+import pytest
+from sqlalchemy import select
+from sqlalchemy.exc import DataError, PendingRollbackError
+
+from gw_team.models.users import User
+
+
+@pytest.mark.asyncio
+async def test_create_user(mock_db_time, session):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            name='test_name',
+            last_name='test_last_name',
+            user_type='client',
+            password='secret',
+            email='test@test.com',
+            disabled=False,
+        )
+        session.add(new_user)
+        await session.commit()
+        user_db = await session.scalar(
+            select(User).where(User.name == 'test_name')
+        )
+        assert asdict(user_db) == {
+            'id': 1,
+            'name': 'test_name',
+            'last_name': 'test_last_name',
+            'user_type': 'client',
+            'password': 'secret',
+            'email': 'test@test.com',
+            'created_at': time,
+            'updated_at': time,
+            'disabled': False,
+        }
+
+
+@pytest.mark.asyncio
+async def test_create_invalid_user(session):
+    invalid_user = User(
+        name='test_name',
+        last_name='test_last_name',
+        user_type='invalid',
+        password='secret',
+        email='test@test.com',
+        disabled=False,
+    )
+    session.add(invalid_user)
+    with pytest.raises(DataError):
+        await session.commit()
+
+    with pytest.raises(PendingRollbackError):
+        await session.scalar(select(User))
