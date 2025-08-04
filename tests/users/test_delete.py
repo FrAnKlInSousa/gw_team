700b48a5030e4d49e2ee879016b958bd3d328005
franklin <frank.fkn.23@gmail.com>
2025-08-04 16:48:19 -0300

    teste no endpoint de deletar usuario

---
+++ tests/users/test_delete.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+
+def test_delete_user(client, user, header_client):
+    response = client.delete(f'/users/{user.id}', headers=header_client)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User successfully deleted'}
+
+
+def test_delete_user_not_found(client, header_admin):
+    response = client.delete('/users/0', headers=header_admin)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_other_user_as_admin(client, user, header_admin):
+    response = client.delete(f'/users/{user.id}', headers=header_admin)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User successfully deleted'}
+
+
+def test_delete_other_user_as_client(client, header_client):
+    response = client.delete('/users/0', headers=header_client)
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'You have no permission'}
