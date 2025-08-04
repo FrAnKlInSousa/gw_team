10ab280feff0c6ac1866a59a598d937383a1b408
franklin <frank.fkn.23@gmail.com>
2025-08-04 17:23:32 -0300

    teste para autenticacao de usuario desativado

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+import pytest
+
 
 def test_authenticate(client, user):
     response = client.post(
@@ -26,3 +28,14 @@ def test_authenticate_error_by_wrong_password(client, token, user):
         data={'username': user.email, 'password': 'wrong_secret'},
     )
     assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+@pytest.mark.asyncio
+async def test_authenticate_error_with_disabled_account(client, custom_user):
+    new_user = await custom_user(disabled=True)
+    response = client.post(
+        '/auth/token',
+        data={'username': new_user.email, 'password': new_user.password},
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'User account is not active'}
