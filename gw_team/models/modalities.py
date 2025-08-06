80ca39ebebe80913757935214151f8a6bfa58c16
franklin <frank.fkn.23@gmail.com>
2025-08-06 14:20:10 -0300

    tabela de modalidade

--- gw_team/models/modalities.py
+++ gw_team/models/modalities.py
@@ -1,15 +1,20 @@
-from pydantic import BaseModel
 from enum import Enum
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from gw_team.models import table_registry
 
 
-class Fight(str, Enum):
+class FightStyle(str, Enum):
     jiu_jitsu='jiu-jitsu'
     capoeira='capoeira'
 
-class Modalidades(BaseModel):
-    luta: Mapped[Fight]
-    int: Mapped[int] = mapped_column(primary_key=True)
-    client: Mapped[int] = relationship(init=False,
-        cascade='all, delete-orphan', lazy='selectin')
+
+@table_registry.mapped_as_dataclass
+class Modality:
+    __tablename__ = 'modalities'
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[FightStyle]
+    users_assoc = Mapped[list['UserModality']] = relationship(
+        back_populates='modality',
+        cascade='all, delete-orphan'
+    )
