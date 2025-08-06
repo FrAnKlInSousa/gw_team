da0b8b58e853ae9058ac2ad874bd0fcb3aa9ea36
franklin <frank.fkn.23@gmail.com>
2025-08-06 16:31:05 -0300

    mudando o tipo da modalidade para str

--- gw_team/models/modalities.py
+++ gw_team/models/modalities.py
@@ -6,16 +6,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from gw_team.models.registry import table_registry
 
 
-class FightStyle(str, Enum):
-    jiu_jitsu = 'jiu-jitsu'
-    capoeira = 'capoeira'
-
-
 @table_registry.mapped_as_dataclass
 class Modality:
     __tablename__ = 'modalities'
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
-    name: Mapped[FightStyle]
+    name: Mapped[str]
     users_assoc: Mapped[List['UserModality']] = relationship(
         back_populates='modality', cascade='all, delete-orphan'
     )
