5ef67600704d7c409b9a74b3ecbf7b30eb2455d0
franklin <frank.fkn.23@gmail.com>
2025-08-23 02:47:47 -0300

    rota para criar agendamento

--- gw_team/routers/appointment.py
+++ gw_team/routers/appointment.py
@@ -1,21 +1,25 @@
+from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from gw_team.database import appointments
 from gw_team.database.engine import db_session
 from gw_team.models.models import User
 from gw_team.schemas.appointments import AppointmentSchema
+from gw_team.schemas.schemas import Message
 from gw_team.security.security import current_user
 
-router = APIRouter()
+router = APIRouter(prefix='/appointments', tags=['appointments'])
 
 T_CurrentUser = Annotated[User, Depends(current_user)]
 T_Session = Annotated[AsyncSession, Depends(db_session)]
 
 
-@router.post('/')
+@router.post('/', status_code=HTTPStatus.CREATED, response_model=Message)
 async def create_schedule(
     user: T_CurrentUser, session: T_Session, data: AppointmentSchema
 ):
-    print(data)
+    await appointments.create(data, user.id, session)
+    return Message(message='Appointment created successfully')
