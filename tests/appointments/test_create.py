4bd58d3a46b55db1f58c10a0c7a4001dd8e5cbbe
franklin <frank.fkn.23@gmail.com>
2025-08-23 03:04:47 -0300

    teste agendamento sem modalidade

--- tests/appointments/test_create.py
+++ tests/appointments/test_create.py
@@ -9,3 +9,12 @@ def test_create_appointment(client, header_client):
     }
     response = client.post('/appointments/', headers=header_client, json=data)
     assert response.status_code == HTTPStatus.CREATED
+
+
+def test_create_appointment_with_wrong_modality(client, header_client):
+    data = {
+        'date': date.today().isoformat(),
+        'modality_id': 0,
+    }
+    response = client.post('/appointments/', headers=header_client, json=data)
+    assert response.status_code == HTTPStatus.NOT_FOUND
