094961032ddbea1cd25c795bea854c01ba1b9300
franklin <frank.fkn.23@gmail.com>
2025-08-25 15:13:45 -0300

    usando classe e tirando notacao async na chamada do teste

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,41 +1,39 @@
 from http import HTTPStatus
 
-import pytest
 
+class TestAuth:
+    def test_authenticate(self, client, user):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},  # type: ignore[attr-defined]
+        )
 
-def test_authenticate(client, user):
-    response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': user.clean_password},  # type: ignore[attr-defined]
-    )
+        assert response.status_code == HTTPStatus.OK
+        assert 'access_token' in response.json()
+        assert 'token_type' in response.json()
 
-    assert response.status_code == HTTPStatus.OK
-    assert 'access_token' in response.json()
-    assert 'token_type' in response.json()
+    def test_authenticate_error_by_wrong_email(self, client, token, user):
+        response = client.post(
+            '/auth/token',
+            data={
+                'username': 'wrong@test.com',
+                'password': user.clean_password,
+            },
+        )
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
 
+    def test_authenticate_error_by_wrong_password(self, client, token, user):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': 'wrong_secret'},
+        )
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
 
-def test_authenticate_error_by_wrong_email(client, token, user):
-    response = client.post(
-        '/auth/token',
-        data={'username': 'wrong@test.com', 'password': user.clean_password},
-    )
-    assert response.status_code == HTTPStatus.UNAUTHORIZED
-
-
-def test_authenticate_error_by_wrong_password(client, token, user):
-    response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': 'wrong_secret'},
-    )
-    assert response.status_code == HTTPStatus.UNAUTHORIZED
-
-
-@pytest.mark.asyncio
-async def test_authenticate_error_with_disabled_account(client, custom_user):
-    new_user = await custom_user(disabled=True)
-    response = client.post(
-        '/auth/token',
-        data={'username': new_user.email, 'password': new_user.password},
-    )
-    assert response.status_code == HTTPStatus.FORBIDDEN
-    assert response.json() == {'detail': 'User account is not active'}
+    def test_authenticate_error_with_disabled_account(self, client, make_user):
+        new_user = make_user(disabled=True)
+        response = client.post(
+            '/auth/token',
+            data={'username': new_user.email, 'password': new_user.password},
+        )
+        assert response.status_code == HTTPStatus.FORBIDDEN
+        assert response.json() == {'detail': 'User account is not active'}
