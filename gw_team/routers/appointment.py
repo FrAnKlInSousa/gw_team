ee3f303293d62b41639f5b2e6a7c871d69f2323c
franklin <frank.fkn.23@gmail.com>
2025-08-26 14:29:50 -0300

    rota para listar agendamentos

--- gw_team/routers/appointment.py
+++ gw_team/routers/appointment.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from gw_team.database import appointments
 from gw_team.database.engine import db_session
 from gw_team.models.models import User
-from gw_team.schemas.appointments import AppointmentSchema
+from gw_team.schemas.appointments import AppointmentList, AppointmentSchema
 from gw_team.schemas.schemas import Message
 from gw_team.security.security import current_user
 
@@ -23,3 +23,11 @@ async def create_schedule(
 ):
     await appointments.create(data, user.id, session)
     return Message(message='Appointment created successfully')
+
+
+@router.get('/', response_model=AppointmentList)
+async def list_appointments(user: T_CurrentUser, session: T_Session):
+    my_appointments = await appointments.get_appointments(
+        session=session, user=user
+    )
+    return {'appointments': my_appointments}
